8255c2e80bfc060bbe688627a3c3acc7e8115aba
David Perez <dperez19279@columbushs.com>
2017-08-08 10:48:13 -0400

    Add empty methods

---
+++ Coding-Club/src/signup.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 19279
+ */
+public class signup {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Coding Club Signup");
+    }
+    public static String capatalizeFirstName(String fName) {
+        // TODO: Return meaningful response
+        
+        return "Justin";
+    }
+    
+    public static String capatalizeLastName(String lName) {
+        // TODO: Return meaningful response
+        
+        return "Parondo";
+    }
+    
+    public static boolean isValidColumbushsEmail(String email) {
+        // TODO: Return meaningful response
+        
+        return true;
+    }
+    
+    public static void prompt(){
+        // TODO: Put prompt and logic in this function
+    }
+    
+    public static void submitInformation(String fName, String lName, String email) {
+        
+    }
+}
